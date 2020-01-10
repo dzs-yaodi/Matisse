78fbedcb5fc3dc0b2a27fe058631b00dbff24a61
xushiyou <xushiyou@sobey.com>
2020-01-10 11:17:44 +0800

    Initial commit

---
+++ matisse/src/main/java/com/zhihu/matisse/internal/utils/UIUtils.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2017 Zhihu Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zhihu.matisse.internal.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.media.MediaMetadataRetriever;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.view.ViewCompat;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class UIUtils {
+
+    public static int spanCount(Context context, int gridExpectedSize) {
+        int screenWidth = context.getResources().getDisplayMetrics().widthPixels;
+        float expected = (float) screenWidth / (float) gridExpectedSize;
+        int spanCount = Math.round(expected);
+        if (spanCount == 0) {
+            spanCount = 1;
+        }
+        return spanCount;
+    }
+
+    public static void translucentStatusBar(Activity activity, boolean hideStatusBarBackground) {
+        Window window = activity.getWindow();
+        //添加Flag把状态栏设为可绘制模式
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        if (hideStatusBarBackground) {
+            //如果为全透明模式，取消设置Window半透明的Flag
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //设置状态栏为透明
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                window.setStatusBarColor(Color.TRANSPARENT);
+            }
+            //设置window的状态栏不可见
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        } else {
+            //如果为半透明模式，添加设置Window半透明的Flag
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //设置系统状态栏处于可见状态
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+        }
+        //view不根据系统窗口来调整自己的布局
+        ViewGroup mContentView = window.findViewById(Window.ID_ANDROID_CONTENT);
+        View mChildView = mContentView.getChildAt(0);
+        if (mChildView != null) {
+            ViewCompat.requestApplyInsets(mChildView);
+        }
+    }
+
+    /**
+     * 获取状态栏高度。
+     *
+     * @param context context
+     * @return 返回获取的状态栏高度
+     */
+    public static float getStatusBarHeight(Context context) {
+        float result = 0;
+        try {
+            int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+            if (resourceId > 0) {
+                result = context.getResources().getDimension(resourceId);
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     *  URI转成 string 地址
+     * @param contentUri
+     * @param context
+     * @return
+     */
+    public static String getRealPathFromUri(Uri contentUri,Context context) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    public static String getVideoTime(int length) {
+
+        String times = "";
+        int minute = length / 60;
+        int ss = length % 60;
+
+        if (minute < 1) {
+            times = "00:";
+        } else {
+            if (minute < 10)
+                times = "0" + minute + ":";
+            else
+                times = minute + ":";
+        }
+
+        if (ss < 10) {
+            times = times + "0" + ss;
+        } else {
+            times = times + ss;
+        }
+
+        return times;
+    }
+
+    /**
+     * 获取视频首帧图片
+     * @param videoPath
+     * @return
+     */
+    public static Bitmap getVideoThumbnail(String videoPath){
+        MediaMetadataRetriever media = new MediaMetadataRetriever();
+        media.setDataSource(videoPath);
+        return media.getFrameAtTime();
+    }
+}
---
+++ matisse/src/main/java/com/zhihu/matisse/ui/CustomVideoActivity.java
@@ -0,0 +1,303 @@
+package com.zhihu.matisse.ui;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import com.zhihu.matisse.R;
+import com.zhihu.matisse.internal.entity.Album;
+import com.zhihu.matisse.internal.entity.Item;
+import com.zhihu.matisse.internal.entity.SelectionSpec;
+import com.zhihu.matisse.internal.model.AlbumCollection;
+import com.zhihu.matisse.internal.model.AlbumMediaCollection;
+import com.zhihu.matisse.internal.model.SelectedItemCollection;
+import com.zhihu.matisse.internal.ui.adapter.CustomAlbumAdapter;
+import com.zhihu.matisse.internal.ui.widget.MediaGridInset;
+import com.zhihu.matisse.internal.utils.UIUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomVideoActivity extends AppCompatActivity implements AlbumCollection.AlbumCallbacks, AlbumMediaCollection.AlbumMediaCallbacks {
+
+    public static final String EXTRA_RESULT_VIDEO_URI = "extra_result_video_uri";
+    private RecyclerView mRecyclerView;
+    private TextView tvCancel;
+    private TextView tvNext;
+    private VideoView videoView;
+    private SelectedItemCollection mSelectedCollection = new SelectedItemCollection(this);
+    private final AlbumCollection mAlbumCollection = new AlbumCollection();
+    private final AlbumMediaCollection mAlbumMediaCollection = new AlbumMediaCollection();
+    private ImageView ivCover;
+    private CustomAlbumAdapter customAlbumAdapter;
+    private List<Item> items = new ArrayList<>();
+    //选中的视频
+    private int selectIndex = 0;
+    private ImageView ivPlay1;
+    private ImageView ivPlay2;
+    private SeekBar mSeekBar;
+    private TextView tvDuration;
+    private CountDownTimer timer;
+    private TextView tvProgress;
+    //视频长度
+    private int length = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_video);
+
+        //设置沉浸式状态栏
+        UIUtils.translucentStatusBar(this, true);
+        //计算出状态栏高度，并设置view留出对应位置
+        int height = (int) UIUtils.getStatusBarHeight(this);
+        View bgView = findViewById(R.id.title_container);
+        bgView.setPadding(0, height, 0, 0);
+        bgView.setBackgroundColor(Color.BLACK);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        tvCancel = findViewById(R.id.tv_cancle);
+        tvNext = findViewById(R.id.tv_next);
+        videoView = findViewById(R.id.videoView);
+        ivCover = findViewById(R.id.image_cover);
+        ivPlay1 = findViewById(R.id.image_play);
+        ivPlay2 = findViewById(R.id.image_play1);
+        mSeekBar = findViewById(R.id.seekbar);
+        tvDuration = findViewById(R.id.duration);
+        tvProgress = findViewById(R.id.tv_progress);
+
+        mSelectedCollection.onCreate(savedInstanceState);
+        initRecyView();
+
+        mAlbumCollection.onCreate(this, this);
+        mAlbumCollection.onRestoreInstanceState(savedInstanceState);
+        mAlbumCollection.loadAlbums();
+
+        mAlbumMediaCollection.onCreate(this, this);
+    }
+
+    private void initRecyView() {
+
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+        int spacing = getResources().getDimensionPixelSize(R.dimen.media_custom_video_grid_spacing);
+        mRecyclerView.addItemDecoration(new MediaGridInset(4, spacing, false));
+
+        customAlbumAdapter = new CustomAlbumAdapter(this);
+        mRecyclerView.setAdapter(customAlbumAdapter);
+
+        setListener();
+
+    }
+
+    private void setListener() {
+        tvCancel.setOnClickListener(view -> finish());
+        ivPlay1.setOnClickListener(v -> videoState());
+        ivPlay2.setOnClickListener(v -> videoState());
+
+        tvNext.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.putExtra(EXTRA_RESULT_VIDEO_URI,UIUtils.getRealPathFromUri(items.get(selectIndex).uri,this));
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+
+        customAlbumAdapter.setOnSelectedListener(position -> {
+
+            for (int i = 0; i < items.size(); i++) {
+                if (i == position) {
+                    items.get(i).isChecked = true;
+                } else {
+                    items.get(i).isChecked = false;
+                }
+            }
+            selectIndex = position;
+            customAlbumAdapter.notifyDataSetChanged();
+            ivCover.setVisibility(View.GONE);
+
+            setVideoDetails(position);
+
+            if (timer != null){
+                timer.cancel();
+                timer = null;
+            }
+
+            videoState();
+            downTime();
+        });
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser){
+                    videoView.seekTo(progress*1000);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void videoState(){
+        if (videoView.isPlaying()){
+            videoView.pause();
+
+            ivPlay1.setImageResource(R.drawable.matisse_video_play_large);
+            ivPlay2.setImageResource(R.drawable.matisse_video_play_small);
+            ivPlay1.setVisibility(View.VISIBLE);
+        }else{
+            videoView.start();
+            downTime();
+            ivPlay1.setVisibility(View.GONE);
+            ivPlay2.setImageResource(R.drawable.matisse_video_stop_small);
+            ivCover.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mSelectedCollection.onSaveInstanceState(outState);
+        mAlbumCollection.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mAlbumCollection.onDestroy();
+        mAlbumMediaCollection.onDestroy();
+
+        if (videoView.isPlaying()){
+            videoView.stopPlayback();
+        }
+
+        if (timer != null){
+            timer.cancel();
+            timer = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onAlbumLoad(Cursor cursor) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+
+            @Override
+            public void run() {
+                cursor.moveToPosition(mAlbumCollection.getCurrentSelection());
+
+                Album album = Album.valueOf(cursor);
+                if (album.isAll() && SelectionSpec.getInstance().capture) {
+                    album.addCaptureCount();
+                }
+                mAlbumMediaCollection.load(album, false);
+            }
+        });
+    }
+
+    @Override
+    public void onAlbumReset() {
+    }
+
+    @Override
+    public void onAlbumMediaLoad(Cursor cursor) {
+        if (cursor.getCount() > 0) {
+            cursor.moveToFirst();
+
+            do {
+                Item item = Item.valueOf(cursor);
+                if (items.size() == 0) {
+                    item.isChecked = true;
+                } else {
+                    item.isChecked = false;
+                }
+                items.add(item);
+            } while (cursor.moveToNext());
+
+            if (items.size() > 0) {
+                customAlbumAdapter.addList(items);
+
+                int screenWidth = getResources().getDisplayMetrics().widthPixels;
+                SelectionSpec.getInstance().imageEngine.loadThumbnail(this, screenWidth,
+                        customAlbumAdapter.mPlaceholder, ivCover, items.get(0).getContentUri());
+
+                setVideoDetails(0);
+            }
+
+        }
+    }
+
+    /**
+     * 设置视频的uri,长度,时间等
+     * @param index
+     */
+    private void setVideoDetails(int index) {
+        videoView.setVideoURI(items.get(index).getContentUri());
+        length = (int) (items.get(index).duration / 1000);
+        mSeekBar.setMax(length);
+        tvDuration.setText(DateUtils.formatElapsedTime(items.get(index).duration / 1000));
+    }
+
+    @Override
+    public void onAlbumMediaReset() {
+
+    }
+
+    private void downTime(){
+
+        long videoLength = length * 1000;
+        timer = new CountDownTimer(videoLength,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                int playTimes = videoView.getCurrentPosition() / 1000;
+
+                if (playTimes >= length){
+                    tvProgress.setText(DateUtils.formatElapsedTime(length));
+                    if (timer != null) {
+                        timer.cancel();
+                        timer = null;
+                    }
+                }else{
+                    tvProgress.setText(DateUtils.formatElapsedTime(playTimes));
+                    mSeekBar.setProgress(playTimes);
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                tvProgress.setText(DateUtils.formatElapsedTime(length));
+                mSeekBar.setProgress(length);
+                if (timer != null) {
+                    timer.cancel();
+                    timer = null;
+                }
+            }
+        };
+        timer.start();
+    }
+}
